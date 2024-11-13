3cc1eefd84c7153a6c21bd16efb38a175fe05b3c
henryparfait <p.uhiriwe@alustudent.com>
2024-11-13 12:30:56 +0200

    SQL

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- Ordering
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Not Null
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL ORDER BY score DESC;
